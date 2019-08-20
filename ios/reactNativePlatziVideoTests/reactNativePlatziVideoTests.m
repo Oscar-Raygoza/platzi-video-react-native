f3ca5fdf055718844435522cf676bb88dd13cd32
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-08-19 23:38:15 -0500

    initialCommit

--- ios/reactNativePlatziVideoTests/reactNativePlatziVideoTests.m
+++ ios/reactNativePlatziVideoTests/reactNativePlatziVideoTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface platziVideoTests : XCTestCase
+@interface reactNativePlatziVideoTests : XCTestCase
 
 @end
 
-@implementation platziVideoTests
+@implementation reactNativePlatziVideoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
